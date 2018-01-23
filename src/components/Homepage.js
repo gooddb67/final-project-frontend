6ae9964028c6c57fbaf0b1fa074f1d42640eb26e
Dave Goodman <davegoodman20@gmail.com>
2018-01-23 11:44:24 -0500

    more homepage styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,39 +16,48 @@ class Homepage extends React.Component{
       'margin-right': '0'
     }
     return(
+    <div>
+      <section className="hero-image">
+        <div className='logo-wrap'>
+          <h1>URLink</h1>
+        </div>
+      </section>
       <div className='wrapper'>
-        <section className="hero-image">
-          <div className='logo-wrap'>
-            <h1>URLink</h1>
-          </div>
-        </section>
         <section className="about-container">
-          <div className="about-item"></div>
+          <div className="about-item">
             <h3>Collect</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat. Maecenas pellentesque ipsum id augue congue, a ultrices est lobortis. Proin fringilla dui ut sem luctus maximus. Cras eu sapien lectus. Cras id nulla
+            <p>
+              Aggregate resources found online
             </p>
-          <div className="about-item"></div>
-            <h3>Annotate</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat. Maecenas pellentesque ipsum id augue congue, a ultrices est lobortis. Proin fringilla dui ut sem luctus maximus. Cras eu sapien lectus. Cras id nulla
-            </p>
-          <div className="about-item"></div>
+          </div>
+          <div className="about-item middle-item">
             <h3>Discover</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat. Maecenas pellentesque ipsum id augue congue, a ultrices est lobortis. Proin fringilla dui ut sem luctus maximus. Cras eu sapien lectus. Cras id nulla
+            <p>
+              Explore articles via the New York Times API
             </p>
+          </div>
+          <div className="about-item">
+            <h3>Analyze</h3>
+            <p>
+              Visualize your findings using Chart.js
+            </p>
+          </div>
         </section>
         <section className="made-with-container">
-          <h2>Made With</h2>
-          <div className="made-with-item">
+
+          <div className="made-with-item react">
             <h3>React</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat. Maecenas pellentesque ipsum id augue congue, a ultrices est lobortis. Proin fringilla dui ut sem luctus maximus. Cras eu sapien lectus. Cras id nulla
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erattus. Cras id nulla
             </p>
           </div>
-          <div className="made-with-item">
+          <div className="made-with-item redux">
             <h3>Redux</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat. Maecenas pellentesque ipsum id augue congue, a ultrices est lobortis. Proin fringilla dui ut sem luctus maximus. Cras eu sapien lectus. Cras id nulla
+            {/* <img src="https://pbs.twimg.com/media/Cg_NJDQWIAAiaK2.jpg" height="90" width="90"></img> */}
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante quam, venenatis nec neque eu, scelerisque finibus enim. Praesent vel magna erat.
             </p>
           </div>
         </section>
+        </div>
       </div>
     )
   }
